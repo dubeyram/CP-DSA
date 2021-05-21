2d0730cc0c5ebff77c81bf3ec262d5148dcd0e06
Ram Gopal Dubey <dubey233310@gmail.com>
2021-05-21 21:57:08 +0530

    Update Selection_sort.py

--- Algorithms/Sorting/Selection_sort.py
+++ Algorithms/Sorting/Selection_sort.py
@@ -1,8 +1,52 @@
-def selectionsort(l):
-    for i in range(len(l)):
-        pos = i
-        for j in range(i,len(l)):
-            if l[j] < l[pos]:
-                pos = j
-        l[pos] , l[i] = l[i], l[pos]
-    return l
+''''
+Algorithm
+Step 1 − Set MIN to location 0
+Step 2 − Search the minimum element in the list
+Step 3 − Swap with value at location MIN
+Step 4 − Increment MIN to point to next element
+Step 5 − Repeat until list is sorted
+
+
+Pseudocode:
+
+procedure selection sort 
+   list  : array of items
+   n     : size of list
+
+   for i = 1 to n - 1
+   /* set current element as minimum*/
+      min = i    
+  
+      /* check the element to be minimum */
+
+      for j = i+1 to n 
+         if list[j] < list[min] then
+            min = j;
+         end if
+      end for
+
+      /* swap the minimum element with the current element*/
+      if indexMin != i  then
+         swap list[min] and list[i]
+      end if
+   end for
+	
+end procedure
+
+'''
+
+
+
+# Python CODE of Selection Sort.
+
+def selection_sort(alist):
+    for i in range(0, len(alist) - 1):
+        smallest = i
+        for j in range(i + 1, len(alist)):
+            if alist[j] < alist[smallest]:
+                smallest = j
+        alist[i], alist[smallest] = alist[smallest], alist[i]
+ 
+
+# Time Complexity − O(n^2)
+# Auxiliary Space − O(1)
