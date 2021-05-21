e61e1ac246c96bd4e70f91548447172eba582a7b
Ram Gopal Dubey <dubey233310@gmail.com>
2021-05-21 21:55:44 +0530

    Update Insertion_sort.py

--- Algorithms/Sorting/Insertion_sort.py
+++ Algorithms/Sorting/Insertion_sort.py
@@ -1,7 +1,59 @@
-def insertionsort(l): 
-    for i in range(len(l)):
-        pos = i
-        while (pos >0) and l[pos] < l[pos-1]:
-            (l[pos], l[pos-1]) = (l[pos-1],l[pos])
-            pos = pos-1
-    return l
+'''
+ALGORITHM : 
+
+Step 1 − If it is the first element, it is already sorted. return 1;
+Step 2 − Pick next element
+Step 3 − Compare with all elements in the sorted sub-list
+Step 4 − Shift all the elements in the sorted sub-list that is greater than the 
+         value to be sorted
+Step 5 − Insert the value
+Step 6 − Repeat until list is sorted
+
+PSEUDOCODE : 
+
+procedure insertionSort( A : array of items )
+   int holePosition
+   int valueToInsert
+	
+   for i = 1 to length(A) inclusive do:
+	
+      /* select value to be inserted */
+      valueToInsert = A[i]
+      holePosition = i
+      
+      /*locate hole position for the element to be inserted */
+		
+      while holePosition > 0 and A[holePosition-1] > valueToInsert do:
+         A[holePosition] = A[holePosition-1]
+         holePosition = holePosition -1
+      end while
+		
+      /* insert the number at hole position */
+      A[holePosition] = valueToInsert
+      
+   end for
+	
+end procedure
+
+'''
+
+
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+
+
+# Time Complexity: O(n*2) 
+# Auxiliary Space: O(1)
