1a38a7e6b4d9ef0c4578ee0fa2b010aa34110ffa
Ramgopal <dubey233310@gmail.com>
2021-09-19 19:31:03 +0530

    Add Arithmetic Operations

---
+++ Arithmetic_operations.py
@@ -0,0 +1,29 @@
+# Addition Operator
+def add (a,b): 
+    return a+b
+
+# Subtract Operator
+def sub(a,b):
+    return a-b
+
+# Multiply Operator
+def mul (a,b):
+    return a*b
+
+# Modules Operator
+def mod(a,b):
+    return a%b
+
+#Divide Operator
+def div (a,b):
+    return a/b
+
+# Exponentiation Operator
+def exp(a,b):
+    return a**b
+
+# Floor division Operator
+def flrdiv(a,b):
+    return a//b
+
+
