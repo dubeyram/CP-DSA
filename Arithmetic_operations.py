3dbfc394a0483d88ebcdbbf862a908a732243160
RamGopal <ram.gopal@thoughts2binary.com>
2022-10-21 23:50:23 +0530

    formatted

--- Arithmetic_operations.py
+++ Arithmetic_operations.py
@@ -1,29 +1,33 @@
 # Addition Operator
-def add (a,b): 
-    return a+b
+def add(a, b):
+    return a + b
+
 
 # Subtract Operator
-def sub(a,b):
-    return a-b
+def sub(a, b):
+    return a - b
+
 
 # Multiply Operator
-def mul (a,b):
-    return a*b
+def mul(a, b):
+    return a * b
+
 
 # Modules Operator
-def mod(a,b):
-    return a%b
+def mod(a, b):
+    return a % b
 
-#Divide Operator
-def div (a,b):
-    return a/b
+
+# Divide Operator
+def div(a, b):
+    return a / b
+
 
 # Exponentiation Operator
-def exp(a,b):
-    return a**b
+def exp(a, b):
+    return a ** b
+
 
 # Floor division Operator
-def flrdiv(a,b):
-    return a//b
-
-
+def flrdiv(a, b):
+    return a // b
